0fff54fcdaf8b422df720d4b3e8ac028d9163344
smartstruggle <natalie.weskamp@googlemail.com>
2026-03-28 16:06:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,40 @@ const setupScreen = document.getElementById("setup-screen");
 const waitingScreen = document.getElementById("waiting-screen");
 const breakScreen = document.getElementById("break-screen");
 
-const startTimeInput = document.getElementById("start-time");
-const endTimeInput = document.getElementById("end-time");
-
+const startHour = document.getElementById("start-hour");
+const startMinute = document.getElementById("start-minute");
+const endHour = document.getElementById("end-hour");
+const endMinute = document.getElementById("end-minute");
 const waitingText = document.getElementById("waiting-text");
 const timer = document.getElementById("timer");
 
 startButton.addEventListener("click", () => {
-const startTime = startTimeInput.value;
-const endTime = endTimeInput.value;
+const startTime = `${startHour.value}:${startMinute.value}`;
+const endTime = `${endHour.value}:${endMinute.value}`;
 
-if (!startTime || !endTime) {
-alert("Bitte beide Zeiten eingeben 🥺");
+
+
+function fillTimeOptions() {
+for (let i = 0; i < 24; i++) {
+const hour = i.toString().padStart(2, "0");
+
+startHour.innerHTML += `<option value="${hour}">${hour}</option>`;
+endHour.innerHTML += `<option value="${hour}">${hour}</option>`;
+}
+
+for (let i = 0; i < 60; i++) {
+const minute = i.toString().padStart(2, "0");
+
+startMinute.innerHTML += `<option value="${minute}">${minute}</option>`;
+endMinute.innerHTML += `<option value="${minute}">${minute}</option>`;
+}
+}
+
+fillTimeOptions();
+
+  
+if (!startHour.value || !startMinute.value || !endHour.value || !endMinute.value) {
+alert("Bitte beide Zeiten vollständig eingeben 🥺");
 return;
 }
 
@@ -57,6 +79,11 @@ clearInterval(interval);
 
 breakScreen.style.display = "none";
 setupScreen.style.display = "block";
+
+startHour.value = "";
+startMinute.value = "";
+endHour.value = "";
+endMinute.value = "";  
 }
 }, 1000);
 }
